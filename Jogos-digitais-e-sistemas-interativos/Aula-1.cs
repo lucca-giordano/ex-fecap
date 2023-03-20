e060750a90b039d6fd84ac840af9d3b4c3b67cce
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-03-20 08:21:58 -0300

    Update Aula-1.cs

--- Jogos-digitais-e-sistemas-interativos/Aula-1.cs
+++ Jogos-digitais-e-sistemas-interativos/Aula-1.cs
@@ -9,8 +9,8 @@ class Program {
   public static void Main (string[] args) {
     Console.WriteLine("Ola mundo 1");
     Console.WriteLine("Ola mundo 2");
-    Console.WriteLine("Ola mundo 3");
-    string nome = "Lucca";
+    Console.WriteLine("Ola mundo 3"); //3 outputs
+    string nome = "Lucca"; //Variavel "nome" = lucca
 
     Console.WriteLine(nome);
   }
