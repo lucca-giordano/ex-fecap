df9c3d3f086b56b3b2f9ef34ca488464e52dd8b3
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-03-20 09:39:23 -0300

    Update Aula-1.cs

--- Jogos-digitais-e-sistemas-interativos/Aula-1.cs
+++ Jogos-digitais-e-sistemas-interativos/Aula-1.cs
@@ -2,18 +2,41 @@
 Imprimir 3 frases
 Declarar uma váriavel "nome"
 Input de nome
+
+Converter string para inteiro
+Testar maioridade
+
 */
 
 using System;
 
 class Program {
   public static void Main (string[] args) {
+
     string nome;
+    int idade = 0;
     
     Console.WriteLine("Ola mundo 1");
     Console.WriteLine("Ola mundo 2");
     Console.WriteLine("Ola mundo 3"); //3 outputs
-    nome = Console.ReadLine(); //input
-    Console.WriteLine("Nome: " + nome); //output
+    Console.Write("Dgite seu nome: ");
+    nome = Console.ReadLine(); //input nome
+
+    Console.Write("Digite sua idade: ");
+    idade = Convert.ToInt32(Console.ReadLine());
+
+    
+    Console.WriteLine("Nome: " + nome); //output nome
+    Console.WriteLine("Idade: " + idade); //output idade
+    if(idade >= 18){
+      Console.WriteLine("Você é maior de idade!");
+    }
+    else if(idade < 18 && idade > 0){
+      Console.WriteLine("Você é menor de idade!");
+    }
+    else{
+      Console.WriteLine("Idade inválida.");
+    }
+    
   }
 }
