b53d562264120a39a5de928c7a8c81166bd14e67
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-03-20 07:50:58 -0300

    Create jogos ditais e aula 1

---
+++ Jogos-digitais-e-sistemas-interativos/Aula-1.cs
@@ -0,0 +1,13 @@
+/* DESAFIO:
+Imprimir 3 frases
+*/
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine("Ola mundo 1");
+    Console.WriteLine("Ola mundo 2");
+    Console.WriteLine("Ola mundo 3");
+  }
+}
