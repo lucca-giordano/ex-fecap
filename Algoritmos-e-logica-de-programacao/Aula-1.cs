76a81a9515ca87fe6c612d17f02ad4f87d2d6ccf
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-03-20 08:12:07 -0300

    Create

---
+++ Algoritmos-e-logica-de-programacao/Aula-1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine(); //comentario
+            Console.WriteLine("FIM DO PROGRAMA!");
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine(i);
+                i+=909;
+            }
+        }
+    }
+}
