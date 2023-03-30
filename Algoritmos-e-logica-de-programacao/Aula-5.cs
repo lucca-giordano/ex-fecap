8da33f7afd04db178cf366b85c350889b529ba13
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-03-30 11:01:30 -0300

    Update Aula-5.cs

--- Algoritmos-e-logica-de-programacao/Aula-5.cs
+++ Algoritmos-e-logica-de-programacao/Aula-5.cs
@@ -1,1 +1,18 @@
+Random rnd = new Random();
 
+int dado1, dado2;
+
+dado1 = (rnd.Next(1, 7));
+dado2 = (rnd.Next(1, 7));
+
+Console.WriteLine("Dado 1: " + dado1);
+Console.WriteLine("Dado 2: " + dado2);
+
+if(dado1 == dado2 || dado2 + dado1 == 7 || dado1 + dado2 == 11)
+{
+    Console.WriteLine("Voce ganhou!");
+}
+else
+{
+    Console.WriteLine("Você perdeu!");
+}
