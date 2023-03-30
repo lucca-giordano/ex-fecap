504ccc2899095fab338e95a3a933254eda6cf86f
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-03-30 08:58:08 -0300

    Create Aula-3.cs

---
+++ Algoritmos-e-logica-de-programacao/Aula-3.cs
@@ -0,0 +1,19 @@
+// Trabalhando com variáveis
+
+//ex1
+double n1, n2, media;
+n1 = 10;
+n2 = 5;
+media = (n1 + n2) / 2;
+Console.WriteLine("Média: " + media);
+
+//ex2
+
+string nomeProduto = "Caderno";
+double preco1, preco2, preco3, precoMedio;
+preco1 = 20;
+preco2 = 30;
+preco3 = 50;
+precoMedio = (preco1 + preco2 + preco3) / 3;
+Console.WriteLine("Produto: " + nomeProduto);
+Console.WriteLine("Media de preços: " + precoMedio);
