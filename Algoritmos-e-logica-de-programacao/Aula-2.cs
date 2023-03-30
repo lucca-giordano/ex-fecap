7ba336b5be0204930dd597a0b5eb684f6edf2566
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-03-30 08:39:30 -0300

    Content - Aula-2.cs

--- Algoritmos-e-logica-de-programacao/Aula-2.cs
+++ Algoritmos-e-logica-de-programacao/Aula-2.cs
@@ -1,1 +1,52 @@
+// INTRODUÇÃO DA AULA
 
+string message1 = "ta bom ta ";
+Console.WriteLine(message1);
+Console.WriteLine(1 + 1);
+Console.WriteLine("1" + "1");
+Console.WriteLine(5 - 2);
+//Console.WriteLine("5" - "2");    //erro
+Console.WriteLine("FE" + "CA" + "P");
+
+
+// Atividade
+
+string message2 = "Bem vindo a FECAP!";
+
+Console.WriteLine(message2);
+
+Console.WriteLine(1+1);
+Console.WriteLine("1" + "1");
+
+// Operações básicas
+
+Console.WriteLine(17 + 3);
+Console.WriteLine(23 - 3);
+Console.WriteLine(10 * 2);
+Console.WriteLine(40 / 2);
+
+// Divisão com float, double e decimal
+
+Console.WriteLine(40.0 / 2.0); //float
+Console.WriteLine(10f / 3f);   //float
+Console.WriteLine(10d / 3d);   //double
+Console.WriteLine(40m / 2m);   //decimal
+
+// Módulo, resto da divisão
+
+Console.WriteLine(40 % 2);
+Console.WriteLine(10 % 3);
+
+// Math
+
+Console.WriteLine(Math.Pow(4, 2)); //potencia
+Console.WriteLine(Math.Sqrt(2));   //raiz quadrada
+
+// Operadores lógicos
+
+Console.WriteLine(17 == 3); //igual
+Console.WriteLine(17 != 3); //diferente
+Console.WriteLine(17 > 3);  //maior que
+Console.WriteLine(17 < 3);  //menor que
+Console.WriteLine(17 >= 3); //maior ou igual a
+Console.WriteLine(17 <= 3); //menor ou igual a
