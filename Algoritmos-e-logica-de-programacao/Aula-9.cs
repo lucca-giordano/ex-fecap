7140305166764a2e7e4a9f3f8a4ce107850c8d7c
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-04-13 10:01:22 -0300

    Update Aula-9.cs

--- Algoritmos-e-logica-de-programacao/Aula-9.cs
+++ Algoritmos-e-logica-de-programacao/Aula-9.cs
@@ -2,17 +2,33 @@ Random rnd= new Random();
 Double[] Preco = new Double[5];
 int i;
 double soma = 0;
-Preco[0] = rnd.NextDouble() * 100;
-Preco[1] = rnd.NextDouble() * 100;
-Preco[2] = rnd.NextDouble() * 100;
-Preco[3] = rnd.NextDouble() * 100;
-Preco[4] = rnd.NextDouble() * 100;
 i = 0;
 while (i < Preco.Length)
 {
+    Preco[i] = rnd.NextDouble() * 100;
     soma = soma + Preco[i];
-    Console.WriteLine("Novo numero: " + Preco[i]);
+    Console.WriteLine("Novo numero: " + Math.Round(Preco[i], 2));
     i++;
 }
 
-Console.WriteLine("A soma é: " + soma);
+Console.WriteLine("A soma é: " + Math.Round(soma, 2));
+Console.WriteLine("A média é: " + Math.Round((soma / Preco.Length), 2));
+
+double maior = Preco[0];
+double menor = Preco[0];
+
+for (i = 0; i < Preco.Length; i++)
+{
+    if (Preco[i] > maior)
+    {
+        maior = Preco[i];
+    }
+
+    if(Preco[i] < menor)
+    {
+        menor = Preco[i];
+    }
+}
+
+Console.WriteLine("O maior número sorteado foi: " + Math.Round(maior, 2));
+Console.WriteLine("O menor número sorteado foi: " + Math.Round(menor, 2));
