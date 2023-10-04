edb43bfe2ab3bc1c2e8173bc0cbdecf873ac4aa8
Lucca Giordano <giordanolucca871@gmail.com>
2023-10-04 07:53:56 -0300

    Create Aula-06.sql
    conteúdo: cláusula ORDER BY

---
+++ Segundo-semestre/Banco-de-Dados-sem2/Aula-06.sql
@@ -0,0 +1,8 @@
+use escritorio;
+
+-- Cláusula ORDER BY --
+-- Esse construtor faz com que a seleção traga os elementos em ordem
+-- Não faz nenhuma alteração na tabela, é apenas para visualização
+
+SELECT * FROM emp ORDER BY job;
+SELECT * FROM emp;
