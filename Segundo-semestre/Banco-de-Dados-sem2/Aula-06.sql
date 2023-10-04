93fb308d10254e6d6d2fb7b64aff7167a78d18a8
Lucca Giordano <giordanolucca871@gmail.com>
2023-10-04 07:54:07 -0300

    Exercícios 1 ao 3

--- Segundo-semestre/Banco-de-Dados-sem2/Aula-06.sql
+++ Segundo-semestre/Banco-de-Dados-sem2/Aula-06.sql
@@ -6,3 +6,12 @@ use escritorio;
 
 SELECT * FROM emp ORDER BY job;
 SELECT * FROM emp;
+
+-- 1. Selecioar de forma ordenada o conteúdo da tabela dept de acordo com o conteúdo da coluna dname em ordem crescente
+SELECT * FROM dept ORDER BY dname;
+
+-- 2. Selecioar de forma ordenada o conteúdo da tabela dept de acordo com o conteúdo da coluna dname em ordem *decrescente*
+SELECT * FROM dept ORDER BY dname DESC;
+
+-- 3. Selecionar de forma ordenada os campos ename e job da tabela emp de acordo com o conteúdo da coluna job e ename em ordem crescente
+SELECT ename, job FROM emp ORDER BY job, ename;
