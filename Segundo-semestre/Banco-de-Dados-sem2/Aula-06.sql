d20008458d671ae8bb5f486a2f52e605adc5e06f
Lucca Giordano <giordanolucca871@gmail.com>
2023-10-04 08:02:29 -0300

    Função SUM e AVG

--- Segundo-semestre/Banco-de-Dados-sem2/Aula-06.sql
+++ Segundo-semestre/Banco-de-Dados-sem2/Aula-06.sql
@@ -14,6 +14,17 @@ SELECT MAX(sal) FROM emp;
 -- É o oposto do MAX, retorna o menor valor do conjunto
 SELECT MIN(sal) FROM emp;
 
+-- Função SUM --
+-- Soma os valores de uma colúna e retorna um valor único
+SELECT SUM(sal) FROM emp;
+SELECT SUM(sal) FROM emp WHERE job="MANAGER";
+
+-- Função AVG --
+-- Calcula e retorna a média dos valores selecionados
+SELECT AVG(sal) FROM emp;
+
+
+
 -- -- -- -- EXERCÍCIOS -- -- -- --
 
 -- 1. Selecioar de forma ordenada o conteúdo da tabela dept de acordo com o conteúdo da coluna dname em ordem crescente
