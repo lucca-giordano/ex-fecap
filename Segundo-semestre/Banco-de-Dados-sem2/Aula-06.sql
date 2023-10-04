eace2a76e78655689f9318fce656cef99f5a5477
Lucca Giordano <giordanolucca871@gmail.com>
2023-10-04 07:57:07 -0300

    Funções MAX e MIN

--- Segundo-semestre/Banco-de-Dados-sem2/Aula-06.sql
+++ Segundo-semestre/Banco-de-Dados-sem2/Aula-06.sql
@@ -3,9 +3,18 @@ use escritorio;
 -- Cláusula ORDER BY --
 -- Esse construtor faz com que a seleção traga os elementos em ordem
 -- Não faz nenhuma alteração na tabela, é apenas para visualização
-
 SELECT * FROM emp ORDER BY job;
 SELECT * FROM emp;
+
+-- Função MAX --
+-- A função MAX analísa um conjunto de valores e retorna o maior valor entre eles
+SELECT MAX(sal) FROM emp;
+
+-- Função MIN --
+-- É o oposto do MAX, retorna o menor valor do conjunto
+SELECT MIN(sal) FROM emp;
+
+-- -- -- -- EXERCÍCIOS -- -- -- --
 
 -- 1. Selecioar de forma ordenada o conteúdo da tabela dept de acordo com o conteúdo da coluna dname em ordem crescente
 SELECT * FROM dept ORDER BY dname;
