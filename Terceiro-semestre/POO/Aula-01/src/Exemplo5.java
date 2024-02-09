08018ee80301a89d6eed1bb467622ef5be362d74
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-08 21:40:22 -0300

    Exemplo 5

---
+++ Terceiro-semestre/POO/Aula-01/src/Exemplo5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exemplo5 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int idade;
+		System.out.println("Informe a idade: ");
+		idade = ler.nextInt();
+		if(idade > 0 && idade < 15) {
+			System.out.println("Não eleitor.");
+		}
+		else if(idade>=16&&idade<=65) {
+			System.out.println("Eleitor!");
+		}
+		else if(idade>65) {
+			System.out.println("Eleitor optativo)");
+		}
+		else {
+			System.out.println("Valor incorreto!");
+		}
+		}
+}
