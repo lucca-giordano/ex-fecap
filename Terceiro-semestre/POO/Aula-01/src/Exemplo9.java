110ce29f9d0520bb7bd44a667e7c94c3ee3a4088
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-08 22:09:10 -0300

    Create Exemplo9.java

---
+++ Terceiro-semestre/POO/Aula-01/src/Exemplo9.java
@@ -0,0 +1,9 @@
+
+public class Exemplo9 {
+	public static void main(String[] args) {
+		int cont;
+		for(cont=0;cont<=10;cont++) {
+			System.out.println("5 X " + cont + " = " + 5 * cont);
+		}
+	}
+}
