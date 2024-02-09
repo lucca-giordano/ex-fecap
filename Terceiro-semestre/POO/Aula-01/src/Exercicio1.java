aff54f44b159efa80f6ae68e090e6a9361ce9d72
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-08 22:20:40 -0300

    Exercicio 1

---
+++ Terceiro-semestre/POO/Aula-01/src/Exercicio1.java
@@ -0,0 +1,13 @@
+
+public class Exercicio1 {
+	  public static void main(String[] args) {
+		    int i;
+		    int num;
+		  for(i=1;i<=10;i++){
+		      for(num=1;num<=10;num++){
+		        System.out.print(i + "X" + num + " = " + i * num + "\n");
+		      }
+		    System.out.println("\n");
+		    }
+		  }
+}
