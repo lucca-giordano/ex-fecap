cf07b25e951aa3c9252117d70b93a8fc9c98ff00
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-15 19:37:41 -0300

    Update Exercicio1.java

--- Terceiro-semestre/POO/Aula-02/src/Exercicio1.java
+++ Terceiro-semestre/POO/Aula-02/src/Exercicio1.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 
 public class Exercicio1 {
-	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);
-		float qtdlitros;
-		double preco;
-		String opcao;
-		
-		System.out.println("Informe a quantidade de litros: ");
-		qtdlitros = ler.nextFloat();
-		
-		System.out.println("Escolha a opção:\nA:Alcool;\nG:Gasolina;\nD:Diesel");
-		opcao = ler.nextLine();
-		
-		switch(opcao) {
-		case "A":
-			preco = qtdlitros * 3.25f;
-			System.out.println("Preço total: " + preco);
-			break;
-			
-		case "G":
-			preco = qtdlitros * 5.58f;
-			System.out.println("Preço total: " + preco);
-			break;
-			
-		case "D":
-			preco = qtdlitros * 5.95f;
-			System.out.println("Preço total: " + preco);
-			break;
-		}
-	}
+	 public static void main(String[] args) {
+		    Scanner ler = new Scanner(System.in);
+		    float qtdlitros;
+		    double preco;
+		    String opcao;
+
+		    System.out.println("Informe a quantidade de litros: ");
+		    
+		    qtdlitros = ler.nextFloat();
+		    ler.nextLine();
+		    System.out.println("Escolha a opção:\nA:Alcool;\nG:Gasolina;\nD:Diesel");
+		    opcao = ler.nextLine();
+		    
+		    if(opcao.equals("A") || opcao.equals("a")){
+		      preco = qtdlitros * 3.25;
+		      System.out.println(qtdlitros + " litros de alcool custam " + preco);
+		    }
+		    else if(opcao.equals("G") || opcao.equals("g")){
+		      preco = qtdlitros * 5.58;
+		      System.out.println(qtdlitros + " litros de gasolina custam " + preco);
+		    }
+		    else if(opcao.equals("D") || opcao.equals("d")){
+		      preco = qtdlitros * 5.95;
+		      System.out.println(qtdlitros + " litros de gasolina custam " + preco);
+		    }
+		    else{
+		      System.out.println("Opção inválida");
+		    }
+		    
+		  }
 }
