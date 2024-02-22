987448871cc14c38e56b23c114ac573a901d0a6e
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-21 21:11:37 -0300

    Aula1

---
+++ Terceiro-semestre/Programacao-android/Aula01/app/src/main/java/br/edu/fecap/aula01/Casa.java
@@ -0,0 +1,16 @@
+package br.edu.fecap.aula01;
+
+public class Casa {
+
+    //Propriedades -> atributos
+    String cor;
+
+    //Ações -> métodos
+
+    void abrirPorta(){
+        System.out.println("Porta aberta!");
+    }
+
+    
+
+}
--- Terceiro-semestre/Programacao-android/Aula01/app/src/main/java/br/edu/fecap/aula01/MainActivity.java
+++ Terceiro-semestre/Programacao-android/Aula01/app/src/main/java/br/edu/fecap/aula01/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ANALOGIA - DECLARAÇÃO DE UM OBJETO
+        Casa minhaCasa = new Casa();
+        minhaCasa.cor = "Vermelha";
+
+        //Exibir o conteúdo (propriedades) do objeto
+        System.out.println(minhaCasa.cor);
+
+        //Chamar método externo
+        minhaCasa.abrirPorta();
+
     }
 }
