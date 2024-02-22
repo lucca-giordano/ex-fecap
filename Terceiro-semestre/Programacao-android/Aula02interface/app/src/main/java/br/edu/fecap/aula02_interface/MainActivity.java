b82757a6b5cdf12038b5a2fc45879711fad85803
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-21 22:13:01 -0300

    Projeto da aula 2

--- Terceiro-semestre/Programacao-android/Aula02interface/app/src/main/java/br/edu/fecap/aula02_interface/MainActivity.java
+++ Terceiro-semestre/Programacao-android/Aula02interface/app/src/main/java/br/edu/fecap/aula02_interface/MainActivity.java
@@ -1,8 +1,10 @@
 package br.edu.fecap.aula02_interface;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+        TextView texto = findViewById(R.id.textResultado);
+
+        int num = new Random().nextInt(100);
+
+        texto.setText("N°: " + num);
     }
 }
