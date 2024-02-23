f6e58a8917a56ec3d2a8879cc7fa06c2adf98a40
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-22 21:37:36 -0300

    Exemplo 1

---
+++ Terceiro-semestre/POO/Aula-03/src/Exemplo1/Carro.java
@@ -0,0 +1,8 @@
+package Exemplo1;
+
+public class Carro {
+    //Lista de atributos
+    String marca, modelo;
+    int ano;
+
+}
---
+++ Terceiro-semestre/POO/Aula-03/src/Exemplo1/Pessoa.java
@@ -0,0 +1,12 @@
+package Exemplo1;
+
+public class Pessoa {
+	//Lista de atributos
+	String nome;
+	int idade;
+	double renda;
+	
+	//Lista de métodos
+    
+	
+}
