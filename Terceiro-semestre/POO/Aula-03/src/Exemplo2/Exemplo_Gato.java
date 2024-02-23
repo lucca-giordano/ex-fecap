b3b82cfb9c25f76862e40005d876ed043f0a6667
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-22 21:52:32 -0300

    Update Exemplo_Gato.java

--- Terceiro-semestre/POO/Aula-03/src/Exemplo2/Exemplo_Gato.java
+++ Terceiro-semestre/POO/Aula-03/src/Exemplo2/Exemplo_Gato.java
@@ -1,5 +1,31 @@
 package Exemplo2;
 
+import java.util.Scanner;
+
 public class Exemplo_Gato {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("Digite o nome do gato: ");
+		gato.nome = ler.nextLine();
+		System.out.println("Digite a cor do gato: ");
+		gato.cor = ler.nextLine();
+		System.out.println("Digite a raça do gato: ");
+		gato.raca = ler.nextLine();
+		System.out.println("Digite a idade do gato: ");
+		gato.idade = ler.nextInt();
+		System.out.println("Digite o peso do gato: ");
+		gato.peso = ler.nextDouble();
+
+		System.out.println("Nome: " + gato.nome);
+		System.out.println("Cor: " + gato.cor);
+		System.out.println("Raça: " + gato.raca);
+		System.out.println("Idade: " + gato.idade);
+		System.out.println("Peso: " + gato.peso);
+		ler.close();	
+
+	}
 
 }
