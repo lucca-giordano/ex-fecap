29083591e136cf494f33f64dd1a1386c821190c4
Lucca Giordano <giordanolucca871@gmail.com>
2024-02-24 21:04:42 -0300

    to tentando

--- Terceiro-semestre/Programacao-android/Exercicio1-megaSena/app/src/main/java/com/example/megasena/MainActivity.java
+++ Terceiro-semestre/Programacao-android/Exercicio1-megaSena/app/src/main/java/com/example/megasena/MainActivity.java
@@ -1,37 +1,42 @@
 package com.example.megasena;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPlay;
+    private TextView textResult;
+
+    Random random = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        int numero1, numero2, numero3, numero4, numero5, numero6;
-        
-        
+
+        btnPlay = findViewById(R.id.btnPlay);
+        textResult = findViewById(R.id.textResult);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                for(int i = 0; i>6; i++){
+                    textResult.setText("Numero: " + i);
+                }
+
+            }
+        });
 
     }
 
     // Método para sortear 6 números diferentes entre 1 e 60
 
-    public void sortearNumeros() {
-        Random random = new Random();
-        int numero;
-
-        for (int i = 0; i < 6; i++) {
-            numero = random.nextInt(60) + 1;
-            System.out.println("Número " + (i + 1) + ": " + numero);
-        }
-    }
-
-    
-    
 
 }
