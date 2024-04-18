4c9c056365948f5aa58de9a441687d187428f12f
Lucca Giordano <giordanolucca871@gmail.com>
2024-04-17 22:08:58 -0300

    aula09

---
+++ Terceiro-semestre/Programacao-android/Aula09/DiferentesTelas/app/src/main/java/br/edu/fecap/telas/MainActivity.java
@@ -0,0 +1,21 @@
+package br.edu.fecap.telas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnChama(View view){
+
+    }
+
+}
